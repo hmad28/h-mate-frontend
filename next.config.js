b6422413065d9197ba00f749b7e7317241d138f5
Hammad <email1.hammad@gmail.com>
2025-10-20 16:47:46 +0700

    change karirku to h-mate

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
+const path = require("path"); // Tambahkan import path
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   // Untuk deployment ke Vercel atau production
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
   },
+
+  // Fix workspace root warning
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 module.exports = nextConfig;
