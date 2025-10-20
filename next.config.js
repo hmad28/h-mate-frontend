f4ca214291f13f32ecc6b6142ce42856901abde3
Hammad <email1.hammad@gmail.com>
2025-10-20 17:39:48 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-const path = require("path"); // Tambahkan import path
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   // Untuk deployment ke Vercel atau production
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
   },
-
-  // Fix workspace root warning
-  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 module.exports = nextConfig;
